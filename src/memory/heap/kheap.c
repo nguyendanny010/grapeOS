dc4c0977f0ff598ef62c273d3e945177ac363a56
Dannny <dannny@pop-os.localdomain>
2022-06-28 13:47:25 -0700

    updated constants

---
+++ src/memory/heap/kheap.c
@@ -0,0 +1,19 @@
+#include "kheap.h"
+#include "heap.h"
+#include "../../config.h"
+#include "../../kernel.h"
+
+struct heap kernel_heap;
+struct heap_table kernel_heap_table; 
+
+void kheap_init(){
+
+    int total_table_entries = GRAPEOS_HEAP_SIZE_BYTES / GRAPEOS_HEAP_BLOCK_SIZE;
+    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY *)(GRAPEOS_HEAP_TABLE_ADDRESS);
+    kernel_heap_table.total = total_table_entries;
+
+    void * end = (void *)(GRAPEOS_HEAP_ADDRESS + GRAPEOS_HEAP_SIZE_BYTES);
+    int res = heap_create(&kernel_heap, (void *)(GRAPEOS_HEAP_ADDRESS), end, &kernel_heap_table);
+
+    if(res< 0) print("Failed to create heap\n");
+}
