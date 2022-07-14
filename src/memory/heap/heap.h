94cd8d88182245a99b5e0b38f776d1be4f82f77c
nguyendanny010 <nguyendanny010@gmail.com>
2022-07-14 12:44:00 -0700

    Moved documentation to header file

--- src/memory/heap/heap.h
+++ src/memory/heap/heap.h
@@ -23,13 +23,79 @@ struct heap{
     void * saddr;
 };
 
+/*
+ * Checks if the table's total number of blocks is correct given end and ptr.
+ */
+static int heap_validate_table(void * ptr, void * end, struct heap_table* table);
+
+/*
+ * Creates the heap.
+ */
 int heap_create(struct heap * heap, void * ptr, void * end, struct heap_table * table);
+
+/*
+ * Aligns the value to the upper block.
+ */
+static uint32_t heap_align_value_to_upper(uint32_t val){
+
+/*
+ * Returns the starting block in the heap where total_blocks are free
+ * @param *heap the current heap.
+ * @param total_blocks the total amount of blocks which must be available.
+ */
 int heap_get_start_block(struct heap * heap, uint32_t total_blocks);
+
+void * heap_malloc_blocks(struct heap * heap, uint32_t total_blocks);
+
+/*
+ * Returns the address of the corresponding block
+ * @param *heap the current heap.
+ * @param block the block whose corresponding address is returned.
+ */
 void * heap_block_to_address(struct heap * heap, int block);
-void * heap_malloc_blocks(struct heap * heap, uint32_t total_blocks);
+
+/*
+ * Marks total_blocks starting from start_block as taken within the heap.
+ * @param *heap the current heap.
+ * @param start_block the block to start from.
+ * @param total_blocks total amount of blocks to mark as taken.
+ */
 void heap_mark_blocks_taken(struct heap * heap, int start_block, int total_blocks);
+
+/*
+ * Retrieves the starting block of total_blocks then marks the blocks starting
+ * from that address as taken. 
+ * @param *heap the current heap.
+ * @param total_blocks total amount of blocks in memory to allocate.
+ */
 void * heap_malloc_blocks(struct heap * heap, uint32_t total_blocks);
+
+/*
+ * Marks blocks from starting_block as free.
+ * @param *heap the current heap.
+ * @param starting_block the block to start from.
+ */
+void heap_mark_blocks_free(struct heap * heap, int starting_block);
+
+/*
+ * Returns the corresponding block number of the address
+ * @param *heap the current heap.
+ * @param *address the address whose block to return.
+ */
+int heap_address_to_block(struct heap * heap, void * address);
+
+/*
+ * Marks size amount of blocks as taken within the heap.
+ * @param *heap the address of the heap.
+ * @param size block size.
+ */
 void * heap_malloc(struct heap * heap, size_t size);
+
+/*
+ * Frees the designated ptr address from the heap.
+ * @param *heap the address of the heap where ptr should be freed from.
+ * @param ptr the address to be freed.
+ */
 void heap_free(struct heap * heap, void * ptr);
 
 #endif
