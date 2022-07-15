2b84f91b59290c21c45cce26aa1eb49f8955981f
nguyendanny010 <nguyendanny010@gmail.com>
2022-07-15 10:43:59 -0700

    Added disk sector read

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 #include <stdint.h>
 #include <stddef.h>
@@ -101,19 +102,12 @@ void kernel_main(){
     // Switch to kernel paging chunk
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
-    char* ptr = kzalloc(4096);
-
-    uint32_t * directory_entry = paging_4gb_chunk_get_directory(kernel_chunk);
-    paging_set(directory_entry, (void *)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
-
     // Enable paging
     enable_paging();
 
-    char* ptr2 = (char*) 0x1000;
-    ptr2[0] = 'A';
-    ptr2[1] = 'B';
-    print(ptr2);
-    print(ptr);
+    char buf[512];
+    disk_read_sector(0, 1, buf);
+
     // Enable the system interrupts
     enable_interrupts();
 }
