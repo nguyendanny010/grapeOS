aaa8b9fadabd5c6896b5e28d4cb1844321664bc9
Dannny <dannny@pop-os.localdomain>
2022-07-14 19:03:32 -0700

    implemented paging support

--- src/memory/paging/paging.h
+++ src/memory/paging/paging.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 #define PAGING_CACHE_DISABLED  0b00010000
 #define PAGING_WRITE_THROUGH   0b00001000
@@ -13,13 +14,18 @@
 #define PAGING_TOTAL_ENTRIES_PER_TABLE 1024
 #define PAGING_PAGE_SIZE 4096
 
+/*
+ * A chunk is a page table. A chunk has a page directory.
+ */
 struct paging_4gb_chunk{
     uint32_t * directory_entry;
 };
 
 struct paging_4gb_chunk * paging_new_4gb(uint8_t flags);
-uint32_t * paging_4gb_chunk_get_directory(struct paging_4gb_chunk * chunk);
 void paging_switch(uint32_t * directory);
 void enable_paging();
+int paging_set(uint32_t* directory, void* virt, uint32_t val);
+uint32_t * paging_4gb_chunk_get_directory(struct paging_4gb_chunk * chunk);
+bool paging_is_aligned(void * addr);
 
 #endif
